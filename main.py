42c88e049c011024907d124cc28ef1ad3fe0721a
Daniel J. Szelogowski <dszelogowski@gmail.com>
2024-07-18 20:43:20 -0500

    Update -- began training model

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import gc
 import os
 import mne
 import sys
@@ -12,7 +13,9 @@ import matplotlib.pyplot as plt
 from utils import *
 from pathlib import Path
 from scipy.signal import welch
-from tensorflow.keras import layers, models, optimizers
+from tensorflow.keras.utils import plot_model
+from sklearn.model_selection import train_test_split
+from tensorflow.keras import callbacks, layers, models, optimizers, regularizers
 
 
 mne.set_log_level('WARNING')
@@ -56,7 +59,7 @@ def process_all_subjects():
     for subject_id in range(1, 22):
         print("Preprocessing and epoching subject", subject_id)
         preprocess_and_epoch(subject_id)
-    print("Finished preprocessing and epoching all subjects. Combining epochs...")
+    print("Finished preprocessing and epoching all subjects.")
 
 
 def combine_epochs():
@@ -105,25 +108,20 @@ def data_generator(eeg_files, audio_features, batch_size=32, verbose=False):
             try:
                 eeg_data, _ = load_eeg_data_memmapped(eeg_file)
                 audio_data = audio_features.get(subject_id)
-
                 if audio_data is None:
                     if verbose:
                         print(f"Skipping {eeg_file} due to missing audio features.")
                     continue
-
                 indices = np.arange(len(eeg_data))
                 np.random.shuffle(indices)
-
                 for start_idx in range(0, len(eeg_data), batch_size):
                     end_idx = min(start_idx + batch_size, len(eeg_data))
                     batch_indices = indices[start_idx:end_idx]
-
-                    # Ensure that the batch indices do not exceed the audio data length
+                    # Ensure the batch indices do not exceed the audio data length
                     if len(audio_data) < len(batch_indices):
                         if verbose:
                             print(f"Audio data for subject {subject_id} is shorter than the batch size.")
                         continue
-
                     yield eeg_data[batch_indices], audio_data[batch_indices]
             except KeyError as e:
                 if verbose:
@@ -131,10 +129,11 @@ def data_generator(eeg_files, audio_features, batch_size=32, verbose=False):
             except Exception as e:
                 if verbose:
                     print(f"An error occurred while processing {eeg_file}: {e}")
+    pass
 
 
 def build_model(input_shape, units=64, dropout_rate=0.5, recurrent_dropout=0.5, num_classes=1,
-                final_activation='linear', loss=None, metrics=None):
+                final_activation='linear', loss=None, metrics=None, l2_reg=0.01):
     if final_activation is None:
         final_activation = 'sigmoid' if num_classes == 1 else 'softmax'
     if loss is None:
@@ -142,24 +141,27 @@ def build_model(input_shape, units=64, dropout_rate=0.5, recurrent_dropout=0.5, 
     if metrics is None:
         metrics = ['accuracy']
     model = models.Sequential([
-        layers.LSTM(units, input_shape=input_shape, recurrent_dropout=recurrent_dropout, return_sequences=True),
-        # layers.BatchNormalization(),
+        layers.Bidirectional(layers.LSTM(units, input_shape=input_shape, recurrent_dropout=recurrent_dropout,
+                                         return_sequences=True, kernel_regularizer=regularizers.l2(l2_reg))),
+        layers.BatchNormalization(),
         layers.Dropout(dropout_rate),
-        # layers.LSTM(units, recurrent_dropout=recurrent_dropout),
-        layers.LSTM(units, return_sequences=False),
-        # layers.BatchNormalization(),
+        layers.Bidirectional(layers.LSTM(units, return_sequences=False, kernel_regularizer=regularizers.l2(l2_reg))),
+        layers.BatchNormalization(),
         layers.Dropout(dropout_rate),
-        layers.Dense(units, activation='relu'),
-        # layers.BatchNormalization(),
-        # layers.Dropout(dropout_rate),
+        layers.Dense(units, activation='relu', kernel_regularizer=regularizers.l2(l2_reg)),
+        layers.BatchNormalization(),
+        layers.Dropout(dropout_rate),
         layers.Dense(num_classes, activation=final_activation)
     ])
-    optimizer = optimizers.Adam()
+    lr_schedule = optimizers.schedules.ExponentialDecay(initial_learning_rate=1e-3, decay_steps=10000, decay_rate=0.9)
+    early_stopping = callbacks.EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
+    optimizer = optimizers.Adam(learning_rate=lr_schedule)
     model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
-    return model
+    return model, [early_stopping]
 
 
 def train_model():
+    print("Training the model...")
     # Load a sample epoch to determine input shape for the model
     # This shape of the data typically returns (n_epochs, n_channels, n_times)
     # For input shape to LSTM, we care about (n_channels, n_times) for a single epoch
@@ -169,22 +171,64 @@ def train_model():
 
     audio_features = extract_audio_features()
     eeg_files = [Path(f"data/epochs/sub-{i}_epochs-epo.fif") for i in range(1, 22)]
-    train_gen = data_generator(eeg_files, audio_features)
+    train_files, val_files = train_test_split(eeg_files, test_size=0.2, random_state=42)
+    train_gen = data_generator(train_files, audio_features, batch_size=32)
+    val_gen = data_generator(val_files, audio_features, batch_size=32)
 
-    model = build_model(input_shape, units=128, dropout_rate=0.5, loss='mse', metrics=['mae'],
-                        recurrent_dropout=0.25, num_classes=1)  # 1 class for continuous valence
+    model, m_callbacks = build_model(input_shape, units=128, dropout_rate=0.5, loss='mse', metrics=['mae'],
+                                     recurrent_dropout=0.25, num_classes=1)  # 1 class for continuous valence
+    model.build(input_shape=(None,) + input_shape)  # Apply input shape to model for summary (because of Bidirectional)
     model.summary()
-    tf.keras.utils.plot_model(model, show_shapes=True, expand_nested=True, to_file='images/lstm_model.png')
+    plot_model(model, show_shapes=True, expand_nested=True,
+               to_file='images/lstm_model.png', show_layer_activations=True, dpi=300)
 
-    history = model.fit(train_gen, steps_per_epoch=100, epochs=10, verbose=1)
+    gc.disable()  # Disable garbage collection to prevent memory issues and stalling during training
+    history = model.fit(train_gen, steps_per_epoch=100, epochs=25, verbose=1,
+                        callbacks=m_callbacks, validation_data=val_gen, validation_steps=25)
     model.save('weights/lstm_model.h5')
+    gc.enable()
 
-    for key in history.history:
-        plt.plot(history.history[key])
+    for key in ['loss', 'mae']:
+        plt.plot(history.history[key], label=f'Training {key.title()}')
+        plt.plot(history.history[f'val_{key}'], label=f'Validation {key.title()}')
         plt.title(f'Model {key.title()}')
         plt.ylabel(key.title())
         plt.xlabel('Epoch')
+        plt.savefig(f'images/model_{key}_history.png')
         plt.show()
+    pass
+
+
+def test_model():
+    print("Testing the model...")
+    audio_features = extract_audio_features()
+    eeg_files = [Path(f"data/epochs/sub-{i}_epochs-epo.fif") for i in range(1, 22)]
+    train_files, val_files = train_test_split(eeg_files, test_size=0.2, random_state=42)
+    _train_gen = data_generator(train_files, audio_features, batch_size=32)
+    val_gen = data_generator(val_files, audio_features, batch_size=32)
+
+    # Time Series Plot
+    model = models.load_model('weights/lstm_model.h5')
+    eeg_data, audio_data = next(val_gen)  # Get a batch of data
+    predictions = model.predict(eeg_data)
+    plt.figure(figsize=(12, 6))  # Plot the first sample in the batch
+    plt.plot(audio_data[0], label='Actual')
+    plt.plot(predictions[0], label='Predicted')
+    plt.legend()
+    plt.title('Actual vs. Predicted EEG Signal')
+    plt.xlabel('Time')
+    plt.ylabel('Signal Value')
+    plt.savefig('images/actual_vs_predicted.png')
+    plt.show()
+
+    # Error Distribution
+    errors = np.abs(predictions - audio_data)  # Calculate the absolute errors
+    plt.hist(errors.flatten(), bins=50)
+    plt.title('Error Distribution')
+    plt.xlabel('Absolute Error')
+    plt.ylabel('Frequency')
+    plt.savefig('images/error_distribution.png')
+    plt.show()
     pass
 
 
@@ -193,3 +237,4 @@ if __name__ == "__main__":
     # process_all_subjects()
     # combine_epochs()
     train_model()
+    test_model()
